14064e747030cc467b0fa24d295ff7cedb4dd216
webserg@gmail.com <y5OkQ=L8>
2019-03-24 19:13:00 +0200

    pytorch perceptron

--- pythonML/notebooks/Pytorch/scripts/nn_loss.py
+++ pythonML/notebooks/Pytorch/scripts/nn_loss.py
@@ -25,5 +25,17 @@ print(output)
 print(output.item())
 output2 = myCrossEntropyLoss(input, target)
 print(output2)
-
+#Mean Squared Error Loss
+mse_loss = nn.MSELoss()
+outputs = torch.randn(3, 5, requires_grad=True)
+print(outputs)
+targets = torch.randn(3, 5)
+loss = mse_loss(outputs, targets)
+print(loss)
+#Categorical Cross-Entropy Loss
+ce_loss = nn.CrossEntropyLoss()
+outputs = torch.randn(3, 5, requires_grad=True)
+targets = torch.tensor([1, 0, 3], dtype=torch.int64)
+loss = ce_loss(outputs, targets)
+print(loss)
 
