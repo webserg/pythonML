630deabb68519cf3857e2af7339ac0bf27862724
webserg@gmail.com <y5OkQ=L8>
2020-03-19 10:12:17 +0200

    pytorch logist regressoin v1

---
+++ pythonML/notebooks/Pytorch/sandbox/logisticRegression.py
@@ -0,0 +1,302 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+from lr_utils import load_dataset
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+device = torch.device("cuda:0")
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset()
+
+# Example of a picture
+index = 27
+print(train_set_x_orig[index].shape)
+plt.imshow(train_set_x_orig[index])
+print("y = " + str(train_set_y[:, index]) + ", it's a '" + classes[np.squeeze(train_set_y[:, index])].decode("utf-8") + "' picture.")
+
+### START CODE HERE ### (≈ 3 lines of code)
+m_train = train_set_x_orig.shape[0]
+m_test = test_set_x_orig.shape[0]
+num_px = train_set_x_orig.shape[1]
+### END CODE HERE ###
+
+print("Number of training examples: m_train = " + str(m_train))
+print("Number of testing examples: m_test = " + str(m_test))
+print("Height/Width of each image: num_px = " + str(num_px))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_set_x shape: " + str(train_set_x_orig.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x shape: " + str(test_set_x_orig.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+
+# Reshape the training and test examples
+
+### START CODE HERE ### (≈ 2 lines of code)
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T
+### END CODE HERE ###
+
+print("train_set_x_flatten shape: " + str(train_set_x_flatten.shape))
+print("train_set_y shape: " + str(train_set_y.shape))
+print("test_set_x_flatten shape: " + str(test_set_x_flatten.shape))
+print("test_set_y shape: " + str(test_set_y.shape))
+print("sanity check after reshaping: " + str(train_set_x_flatten[0:5, 0]))
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+# Hyper-parameters
+input_size = train_set_x_flatten.shape[0]
+num_classes = 10
+num_epochs = 200
+batch_size = 100
+learning_rate = 0.005
+
+print("input_size = " + str(input_size))
+
+
+# GRADED FUNCTION: sigmoid
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Arguments:
+    z -- A scalar or numpy array of any size.
+
+    Return:
+    s -- sigmoid(z)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    s = 1 / (1 + torch.exp(-z))
+    ### END CODE HERE ###
+
+    return s
+
+
+# GRADED FUNCTION: initialize_with_zeros
+
+def initialize_with_zeros(dim):
+    """
+    This function creates a vector of zeros of shape (dim, 1) for w and initializes b to 0.
+
+    Argument:
+    dim -- size of the w vector we want (or number of parameters in this case)
+
+    Returns:
+    w -- initialized vector of shape (dim, 1)
+    b -- initialized scalar (corresponds to the bias)
+    """
+
+    ### START CODE HERE ### (≈ 1 line of code)
+    w = torch.zeros(dim, 1)
+    b = 0
+    ### END CODE HERE ###
+
+    assert (w.shape == (dim, 1))
+    assert (isinstance(b, float) or isinstance(b, int))
+
+    return w, b
+
+
+def propagate(w, b, X, Y):
+    """
+    Implement the cost function and its gradient for the propagation explained above
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat) of size (1, number of examples)
+
+    Return:
+    cost -- negative log-likelihood cost for logistic regression
+    dw -- gradient of the loss with respect to w, thus same shape as w
+    db -- gradient of the loss with respect to b, thus same shape as b
+
+    Tips:
+    - Write your code step by step for the propagation. np.log(), np.dot()
+    """
+
+    m = X.shape[1]
+
+    # FORWARD PROPAGATION (FROM X TO COST)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    A = sigmoid(torch.mm(w.t(), X) + b)  # compute activation
+    cost = - 1 / m * torch.sum(Y * torch.log(A) + (1 - Y) * torch.log(1 - A))  # compute cost
+    ### END CODE HERE ###
+
+    # BACKWARD PROPAGATION (TO FIND GRAD)
+    ### START CODE HERE ### (≈ 2 lines of code)
+    # cost.requires_grad_(True)
+    cost.backward()
+    dw = w.grad
+    db = b.grad
+    ### END CODE HERE ###
+
+    # assert (dw.shape == w.shape)
+    # assert (db.dtype == float)
+    # cost = np.squeeze(cost)
+    # assert (cost.shape == ())
+    #
+    grads = {"dw": dw,
+             "db": db}
+
+    return grads, cost
+
+
+def test_propagate():
+    w, b, X, Y = torch.tensor([[1.], [2.]], requires_grad=True), torch.tensor([2.], requires_grad=True), torch.tensor(
+        [[1., 2., -1.], [3., 4., -3.2]]), torch.tensor([[1.0, 0.0, 1.0]])
+    grads, cost = propagate(w, b, X, Y)
+    print("dw = " + str(grads["dw"]))
+    print("db = " + str(grads["db"]))
+    print("cost = " + str(cost))
+
+
+# GRADED FUNCTION: optimize
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost=False):
+    """
+    This function optimizes w and b by running a gradient descent algorithm
+
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of shape (num_px * num_px * 3, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
+    num_iterations -- number of iterations of the optimization loop
+    learning_rate -- learning rate of the gradient descent update rule
+    print_cost -- True to print the loss every 100 steps
+
+    Returns:
+    params -- dictionary containing the weights w and bias b
+    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
+
+    Tips:
+    You basically need to write down two steps and iterate through them:
+        1) Calculate the cost and the gradient for the current parameters. Use propagate().
+        2) Update the parameters using gradient descent rule for w and b.
+    """
+
+    costs = []
+
+    for i in range(num_iterations):
+
+        # Cost and gradient calculation (≈ 1-4 lines of code)
+        ### START CODE HERE ###
+        grads, cost = propagate(w, b, X, Y)
+        ### END CODE HERE ###
+
+        # Retrieve derivatives from grads
+        dw = grads["dw"]
+        db = grads["db"]
+
+        # update rule (≈ 2 lines of code)
+        ### START CODE HERE ###
+        w = w - learning_rate * dw
+        b = b - learning_rate * db
+
+        ### END CODE HERE ###
+
+        # Record the costs
+        if i % 100 == 0:
+            costs.append(cost)
+
+        # Print the cost every 100 training examples
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    params = {"w": w,
+              "b": b}
+
+    grads = {"dw": dw,
+             "db": db}
+
+    return params, grads, costs
+
+
+class LR_NET(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.input = nn.Linear(input_size, 400)
+        self.hidden = nn.Linear(400, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Pass the input tensor through each of our operations
+        x = self.input(x)
+        x = self.hidden(x)
+        x = self.sigmoid(x)
+        return x
+
+    def predict(self, x):
+        pred = self.forward(x)
+        return pred >= 0.5
+
+
+##======================================================================================
+
+# test_propagate()
+
+model = LR_NET().to(device)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+criterion = nn.BCELoss()
+# optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# criterion = nn.CrossEntropyLoss()
+loss_history = []
+epoch_history = []
+# Train the model
+
+
+for epoch in range(num_epochs):
+    model.train()
+    # Convert numpy arrays to torch tensors
+    # Forward pass
+    inputs = torch.from_numpy(train_set_x).t().to(device, torch.float)
+    targets = torch.from_numpy(train_set_y).t().to(device, torch.float)
+    optimizer.zero_grad()
+    y_hat = model(inputs)
+    loss = criterion(y_hat, targets)
+    # Backward and optimize
+    loss.backward()
+    optimizer.step()
+
+    # if (epoch + 1) % 10 == 0:
+    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, num_epochs, loss.item()))
+    loss_history.append(loss.item())
+    epoch_history.append(epoch)
+
+
+# Test the model
+# In test phase, we don't need to compute gradients (for memory efficiency)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    images = torch.from_numpy(test_set_x).to(device, torch.float)
+    labels = torch.from_numpy(test_set_y).t().to(device, torch.long)
+    outputs = model.predict(images.t())
+    Y_prediction =outputs.to(device, torch.long)
+    # _, predicted = torch.max(outputs.data, 1)
+    total = labels.size(0)
+    correct = (Y_prediction == labels).sum().item()
+    print('Accuracy of the network on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Plot the graph
+# predicted = model(torch.from_numpy(test_set_x_flatten)).detach().numpy()
+# plt.plot(epoch_history, loss_history, 'ro', label='Original data')
+# plt.plot(test_set_x_flatten, predicted, label='Fitted line')
+# plt.legend()
+# plt.show()
+
+# Save the model checkpoint
+torch.save(model.state_dict(), 'pytorch_one_layer_LR.ckpt')
