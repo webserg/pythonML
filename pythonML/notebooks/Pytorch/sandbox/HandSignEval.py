642a876a2798c39323c411ae5eed0c5761254ccc
webserg <webserg@gmail.com>
2020-03-29 23:18:06 +0300

    comparison test result predicted and target

--- pythonML/notebooks/Pytorch/sandbox/HandSignEval.py
+++ pythonML/notebooks/Pytorch/sandbox/HandSignEval.py
@@ -17,18 +17,27 @@ from pythonML.notebooks.Pytorch.sandbox.HandSignConvNet import *
 device = torch.device("cuda:0")
 
 modelFilePath = "C:/git/pythonML/pythonML/notebooks/Pytorch/sandbox/pytorch_HandSign.ckpt"
-model = HandSignConvNet().to(device)
-model.load_state_dict(torch.load(modelFilePath))
+# model = HandSignConvNet().to(device)
+model = torch.load(modelFilePath).to(device)
 model.eval()
 
 test_dataset = HandSignDataset('C:/git/pythonML/pythonML/notebooks/courseraML/convolution-week1/datasets/test_signs.h5', "test_set_",
                                True)
 
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=64, shuffle=False)
-
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=len(test_dataset), shuffle=False)
+wrong_elements = []
 for images, labels in test_loader:
     print(labels)
     outputs = model(images.to(device, torch.float))
     _, predicted = torch.max(outputs.data, 1)
     print(predicted)
     print("-----------------")
+    labels = labels.to(device)
+    comparison = labels != predicted
+    wrong_elements = (comparison == 1).nonzero()
+
+wrong_elements = wrong_elements.reshape(-1)
+for idx in wrong_elements:
+    print(predicted[idx])
+    test_dataset.show(idx)
+
