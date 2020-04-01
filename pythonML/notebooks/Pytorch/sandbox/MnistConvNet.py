09f41e2c0a75ef69c63551ffebb70b001c97a95a
webserg <webserg@gmail.com>
2020-04-01 22:18:53 +0300

    98.58%

--- pythonML/notebooks/Pytorch/sandbox/MnistConvNet.py
+++ pythonML/notebooks/Pytorch/sandbox/MnistConvNet.py
@@ -31,34 +31,51 @@ class MnistConvNet(nn.Module):
         out = self.fc(x)
         return out
 
+    def train_net(self, train_loader, criterion, optimizer):
+        print(self)
+        # Train the model
+        total_step = len(train_loader)
+        for epoch in range(num_epochs):
+            for i, (images, labels) in enumerate(train_loader):
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = self(images)
+                loss = criterion(outputs, labels)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                if (i + 1) % 100 == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                          .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
 
 if __name__ == '__main__':
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # Hyper parameters
-    num_epochs = 5
+    num_epochs = 2
     num_classes = 10
     batch_size = 100
     learning_rate = 0.001
 
     # MNIST dataset
     train_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                           train=True,
-                                           transform=transforms.ToTensor(),
-                                           download=True)
+                                               train=True,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
 
     test_dataset = torchvision.datasets.MNIST(root='../../data/',
-                                          train=False,
-                                          transform=transforms.ToTensor())
+                                              train=False,
+                                              transform=transforms.ToTensor())
 
     # Data loader
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=True)
+                                               batch_size=batch_size,
+                                               shuffle=True)
 
     test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
-                                          batch_size=batch_size,
-                                          shuffle=False)
+                                              batch_size=batch_size,
+                                              shuffle=False)
 
     model = MnistConvNet(num_classes).to(device)
 
@@ -66,25 +83,7 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    # Train the model
-    total_step = len(train_loader)
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
-
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-            if (i + 1) % 100 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+    model.train_net(train_loader, criterion, optimizer)
 
     # Test the model
     model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
