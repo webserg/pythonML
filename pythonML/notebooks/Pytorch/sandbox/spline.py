312c6188fbf7d605bda53d1966519793883f1ba5
webserg <webserg@gmail.com>
2020-04-17 22:41:22 +0300

    normal distr

--- pythonML/notebooks/Pytorch/sandbox/spline.py
+++ pythonML/notebooks/Pytorch/sandbox/spline.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 import torch
 import numpy as np
 import pylab as pl
+import math
+
+
+def normal_pdf(x, mu=0, sigma=1):
+    sqrt_two_pi = math.sqrt(2 * math.pi)
+    return (math.exp(-(x - mu) ** 2 / 2 / sigma ** 2) / (sqrt_two_pi * sigma))
+
 
 if __name__ == '__main__':
     x = np.arange(-3, 10, 0.05)
@@ -22,4 +29,13 @@ if __name__ == '__main__':
 
     plt.plot(xbar, ybar, color='green')
     plt.legend()
+    plt.show()
+
+    xs = [x / 10.0 for x in range(-50, 50)]
+    plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='mu=0,sigma=1')
+    plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='mu=0,sigma=2')
+    plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label='mu=0,sigma=0.5')
+    plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='mu=-1,sigma=1')
+    plt.legend()
+    plt.title("Various Normal pdfs")
     plt.show()
