89375c889baaeabd4957f69cc1df1ab51662f866
webserg <webserg@gmail.com>
2020-12-09 22:50:47 +0200

    long landing

--- pythonML/notebooks/Pytorch/sandbox/reinforcement_learning/LunarLanderPolicyGradient.py
+++ pythonML/notebooks/Pytorch/sandbox/reinforcement_learning/LunarLanderPolicyGradient.py
@@ -16,12 +16,13 @@ from matplotlib import pylab as plt
 
 
 class NetConfig:
-    file_path = '../models/LunarLanderPolicyGradient.pt'
+    file_path = '../models/LunarLanderPolicyGradient3.pt'
     learning_rate = 1e-3
     l1 = 8
-    l2 = 150
-    l3 = 80
-    l4 = 4
+    l2 = 16
+    l3 = 16
+    l4 = 16
+    l5 = 4
 
     def __init__(self):
         pass
@@ -35,6 +36,7 @@ class PolicyGradientNet(nn.Module):
         self.fc1 = nn.Linear(config.l1, config.l2)
         self.fc2 = nn.Linear(config.l2, config.l3)
         self.fc3 = nn.Linear(config.l3, config.l4)
+        self.fc4 = nn.Linear(config.l4, config.l5)
         self.optimizer = torch.optim.Adam(self.parameters(), lr=config.learning_rate)
         self.losses = []  # A
 
@@ -44,7 +46,7 @@ class PolicyGradientNet(nn.Module):
         x = F.leaky_relu_(self.fc1(x))
         x = F.leaky_relu_(self.fc2(x))
         x = F.leaky_relu_(self.fc3(x))
-        x = F.softmax(x, dim=0)  # COutput is a softmax probability distribution over actions
+        x = F.softmax(self.fc4(x), dim=0)  # COutput is a softmax probability distribution over actions
         return x
 
     def fit(self, x, target):
@@ -52,8 +54,8 @@ class PolicyGradientNet(nn.Module):
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-
-        self.losses.append(loss.item())
+        loss = loss / len(x)
+        self.losses.append(loss.detach().item())
 
     def save(self):
         torch.save(self.state_dict(), self.config.file_path)
@@ -83,7 +85,7 @@ if __name__ == '__main__':
     env = gym.make('LunarLander-v2')
     config = NetConfig()
     model = PolicyGradientNet(config)
-    MAX_EPISODES = 2000
+    MAX_EPISODES = 3000
     gamma = 0.99
 
     time_steps = []
@@ -92,8 +94,9 @@ if __name__ == '__main__':
         done = False
         transitions = []  # list of state, action, rewards
         total_reward = 0
-
+        step_counter=0
         while not done:
+            step_counter+=1
             act_prob = model(curr_state)
             action = np.random.choice(np.array([0, 1, 2, 3]), p=act_prob.data.numpy())
             prev_state = curr_state
