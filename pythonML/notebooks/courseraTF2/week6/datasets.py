c64ba77cd71cfc9ddc6c2349daf830536b1c4356
webserg <webserg@gmail.com>
2021-04-04 11:11:30 +0300

    tf2 datasets

--- pythonML/notebooks/courseraTF2/week6/datasets.py
+++ pythonML/notebooks/courseraTF2/week6/datasets.py
@@ -1,6 +1,32 @@
 import tensorflow as tf
+from tensorflow.keras import Model
 from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+
+class CiFar10ConvNet(Model):
+    def __init__(self):
+        super(CiFar10ConvNet, self).__init__()
+        self.conv1 = Conv2D(32, 3, activation='relu')
+        self.flatten = Flatten()
+        self.d1 = Dense(128, activation='relu')
+        self.d2 = Dense(10)
+
+    def call(self, x):
+        x = self.conv1(x)
+        x = self.flatten(x)
+        x = self.d1(x)
+        return self.d2(x)
+
+
+def rescale(image, label):
+    return image / 255, label
+
+
+def label_filter(image, label):
+    return tf.squeeze(label) != 9
+
 
 if __name__ == '__main__':
     dataset = tf.data.Dataset.from_tensor_slices([1, 2, 3, 4, 5, 6])
@@ -36,3 +62,21 @@ if __name__ == '__main__':
                                              output_types=(tf.float32, tf.int32),
                                              output_shapes=([32, 32, 32, 3], [32, 1]))
     print(dataset.element_spec)
+
+    dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+
+    dataset = dataset.batch(16)
+    print(dataset.element_spec)
+
+    dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+    dataset.map(rescale)
+    dataset.filter(label_filter)
+    dataset.shuffle(100)
+
+    dataset = dataset.batch(16, drop_remainder=True)
+    print(dataset.element_spec)
+
+    dataset = dataset.repeat(10)  # train for 10 epoch
+
+    model = CiFar10ConvNet()
+    history = model.hit(dataset)
